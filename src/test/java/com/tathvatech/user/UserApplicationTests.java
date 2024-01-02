11be071c59b2bb79ebcab447d83c2b1afb244a8e
silpy sagar <silpy.sagar@epsilon.com>
2024-01-02 14:54:38 +0530

    initialisation

---
+++ src/test/java/com/tathvatech/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tathvatech.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
