bb34796245cd52e687a0234103a4da9464f1b244
silpy sagar <silpy.sagar@epsilon.com>
2024-02-27 12:19:48 +0530

    In Progress

---
+++ TestSutraUsers/src/main/java/com/tathvatech/site/entity/SiteFilter.java
@@ -0,0 +1,50 @@
+package com.tathvatech.site.entity;
+
+import com.tathvatech.ts.core.authorization.Role;
+import com.tathvatech.ts.core.project.ProjectOID;
+
+
+public class SiteFilter
+{
+	private String searchString; // this is usually a user entered string, match this to site name in case of Site
+	private int[] sitePks;
+	private String siteName;
+	private ProjectOID projectOID;
+	private Role[] validRoles;
+	
+	public String getSearchString() {
+		return searchString;
+	}
+	public void setSearchString(String searchString) {
+		this.searchString = searchString;
+	}
+	public int[] getSitePks() {
+		return sitePks;
+	}
+	public void setSitePks(int[] sitePks) {
+		this.sitePks = sitePks;
+	}
+	public String getSiteName() {
+		return siteName;
+	}
+	public void setSiteName(String siteName) {
+		this.siteName = siteName;
+	}
+	public ProjectOID getProjectOID()
+	{
+		return projectOID;
+	}
+	public void setProjectOID(ProjectOID projectOID)
+	{
+		this.projectOID = projectOID;
+	}
+	public Role[] getValidRoles()
+	{
+		return validRoles;
+	}
+	public void setValidRoles(Role[] validRoles)
+	{
+		this.validRoles = validRoles;
+	}
+	
+}
---
+++ TestSutraUsers/src/main/java/com/tathvatech/site/processor/SiteQuerySecurityProcessor.java
@@ -0,0 +1,96 @@
+package com.tathvatech.site.processor;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import com.tathvatech.ts.core.UserContext;
+import com.tathvatech.ts.core.accounts.User;
+import com.tathvatech.ts.core.authorization.AuthorizationDelegate;
+import com.tathvatech.ts.core.authorization.Role;
+import com.tathvatech.ts.core.authorization.RoleRepository;
+import com.tathvatech.ts.core.common.EntityTypeEnum;
+import com.tathvatech.ts.core.common.service.CommonServicesDelegate;
+import com.tathvatech.ts.core.sites.SiteRolesEnum;
+
+public class SiteQuerySecurityProcessor {
+
+	public void addAuthorizationFilterParams(UserContext context, SiteFilter siteFilter) throws Exception 
+	{
+		if(User.USER_PRIMARY.equals(context.getUser().getUserType()))
+		{
+			return;
+		}
+		if(siteFilter.getValidRoles() != null)
+		{
+			List<Integer> returnList = new ArrayList();
+			
+			// site Level roles
+			List<Role> siteLevelRoles = new ArrayList();
+			List<Role> projectSiteLevelRoles = new ArrayList<>();
+			for (int i = 0; i < siteFilter.getValidRoles().length; i++)
+			{
+				Role aRole = siteFilter.getValidRoles()[i];
+				EntityTypeEnum type = RoleRepository.getInstance().getAuthorizableEntityTypeForRoleId(aRole.getId());
+				if(EntityTypeEnum.Site == type)
+				{
+					siteLevelRoles.add(aRole);
+				}
+				else if(EntityTypeEnum.ProjectSiteConfig == type)
+				{
+					projectSiteLevelRoles.add(aRole);
+				}
+			}
+			if(siteLevelRoles.size() > 0)
+			{
+				List<Integer> sitePks = new AuthorizationDelegate().getEntitiesWithRole(context, EntityTypeEnum.Site, siteLevelRoles.toArray(new Role[siteLevelRoles.size()]));
+				returnList.addAll(sitePks);
+			}
+			if(projectSiteLevelRoles.size() > 0)
+			{
+				List<Integer> projectSitePks = new AuthorizationDelegate().getEntitiesWithRole(context, EntityTypeEnum.ProjectSiteConfig, projectSiteLevelRoles.toArray(new Role[projectSiteLevelRoles.size()]));
+				for (Iterator iterator = projectSitePks.iterator(); iterator.hasNext();)
+				{
+					Integer aProjectSitePk = (Integer) iterator.next();
+					ProjectSiteConfig aPConfig = (ProjectSiteConfig)new CommonServicesDelegate().getObjectByPk(ProjectSiteConfig.class, aProjectSitePk);
+					if(aPConfig != null)
+					{
+						if(!(returnList.contains(aPConfig.getSiteFk())))
+							returnList.add(aPConfig.getSiteFk());
+					}
+				}
+			}
+			if(returnList.size() == 0)
+				returnList.add(-1); // no access to any sites
+			
+			siteFilter.setSitePks(ArrayUtils.toPrimitive(returnList.toArray(new Integer[returnList.size()])));
+			return;
+		}
+		else
+		{
+			// this is kept here for backward compatability.. All calls to getSites should use the validRoles to get the sites with those roles.
+			List<Integer> list = new AuthorizationDelegate().getEntitiesWithRole(context, EntityTypeEnum.Site, SiteRolesEnum.HSECoordinator);
+			list.addAll(new AuthorizationDelegate().getEntitiesWithRole(context, EntityTypeEnum.Site, SiteRolesEnum.HSEDirector));
+			list.addAll(new AuthorizationDelegate().getEntitiesWithRole(context, EntityTypeEnum.Site, SiteRolesEnum.NCRUser));
+			list.addAll(new AuthorizationDelegate().getEntitiesWithRole(context, EntityTypeEnum.Site, SiteRolesEnum.SiteAdmin));
+			if(list == null || list.size() == 0)
+			{
+				siteFilter.setSitePks(new int[]{((User)context.getUser()).getSitePk()});
+				return;
+			}
+			else
+			{
+				int[] sitePks = new int[list.size()];
+				for (int i=0; i<list.size(); i++) 
+				{
+					sitePks[i] = list.get(i);
+				}
+				siteFilter.setSitePks(sitePks);
+				return;
+			}
+		}
+	}
+
+}
