6d6830981d43c9844cd65971e59e9f9d939d9776
silpy sagar <silpy.sagar@epsilon.com>
2024-02-28 10:22:40 +0530

    in progress

---
+++ TestSutraUsers/src/main/java/com/tathvatech/site/oid/ProjectSiteConfigOID.java
@@ -0,0 +1,26 @@
+package com.tathvatech.site.oid;
+
+import com.tathvatech.ts.core.common.EntityTypeEnum;
+import com.tathvatech.ts.core.common.OID;
+
+public class ProjectSiteConfigOID extends OID{
+
+	public ProjectSiteConfigOID(int pk, String displayText)
+	{
+		super(pk, EntityTypeEnum.ProjectSiteConfig, displayText);
+	}
+
+	@Override
+	public int hashCode() {
+		return getPk();
+	}
+
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if(obj instanceof ProjectSiteConfigOID && ((ProjectSiteConfigOID)obj).getPk() == getPk())
+			return true;
+		return false;
+	}
+	
+}
